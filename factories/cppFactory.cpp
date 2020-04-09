0eb00ebe3824d3517c24a8fa9176a4face136a2a
IosifGuzeev <Iosif.Guzeev@perfectart.com>
2020-04-09 23:37:48 +0700

    Added structure of the main class

--- factories/cppFactory.cpp
+++ factories/cppFactory.cpp
@@ -1,1 +1,52 @@
 #include "cppFactory.h"
+
+CppClassUnit::CppClassUnit(std::string name)
+{
+    this->name = name;
+    subUnits["public"] = UnitVector();
+    subUnits["private"] = UnitVector();
+    subUnits["protected"] = UnitVector();
+}
+
+void CppClassUnit::addSubUnit(const std::shared_ptr<Unit> &subUnit, Flags accessType)
+{
+    std::string key;
+    switch (accessType) {
+    case PUBLIC:
+    {
+        key = "public";
+        break;
+    }
+    case PRIVATE:
+    {
+        key = "private";
+        break;
+    }
+    case PROTECTED:
+    {
+        key = "protected";
+        break;
+    }
+    default:
+    {
+        std::runtime_error("Wrong access type!");
+        break;
+    }
+    }
+    subUnits[key].push_back(subUnit);
+}
+
+std::string CppClassUnit::Compile(unsigned int level)
+{
+    std::cout << "class " << name << std::endl;
+    std::cout << "{\n";
+    for (auto &unitsVec: subUnits)
+    {
+        std::cout << unitsVec.first << std::endl;
+        for(auto &e: unitsVec.second)
+        {
+            std::cout << e->Compile(level + 1) << std::endl;
+        }
+    }
+    std::cout << "};\n";
+}
--- factories/cppFactory.h
+++ factories/cppFactory.h
@@ -2,11 +2,34 @@
 #define ABSTRACTFACTORY_H
 #include <interfaces.h>
 #include <memory>
+#include <string>
+#include <vector>
+#include <map>
 
 class CppFactory : public AbstractFactory
 {
     std::shared_ptr<ClassUnit> GetClassUnit(std::string name, unsigned int flags) override;
     std::shared_ptr<MethodUnit> GetMethodUnit(std::string name, std::string returnType, unsigned int flags) override;
+};
+
+class CppClassUnit : public ClassUnit
+{
+    using UnitVector = std::vector<std::shared_ptr<Unit>>;
+
+    std::string name;
+    std::map<std::string, UnitVector> subUnits;
+
+public:
+
+    enum AccessType{
+        PUBLIC = 0,
+        PRIVATE = 1,
+        PROTECTED = 2,
+    };
+
+    CppClassUnit(std::string name);
+    void addSubUnit(const std::shared_ptr<Unit>& subUnit, Flags accessType) override;
+    std::string Compile(unsigned int level = 0) override;
 };
 
 #endif // ABSTRACTFACTORY_H
